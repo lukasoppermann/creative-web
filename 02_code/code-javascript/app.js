3ef9b305a3ff7a69b5138778a1621c11000abe7b
Lukas Oppermann <lukas@vea.re>
2016-06-17 16:09:13 +0200

    javascript & transitions

---
+++ 02_code/code-javascript/app.js
@@ -0,0 +1,11 @@
+/*
+  popup
+ */
+// alert('Hello');
+// comment: write something to console
+// open in chrome with cmd + alt + i
+// console.log('Hello');
+var name = 'Matthias';
+alert('Hello '+name);
+var number = 1;
+alert(number+12);
